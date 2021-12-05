661908030fd4a97fe9432897c1f33bb696ae8a87
George Choustoulakis <georgechoustoulakis@hotmail.com>
2021-12-05 10:47:04 +0100

    Add day5 part one

---
+++ src/day5/vents.ts
@@ -0,0 +1,69 @@
+import {readFileSync} from "fs";
+import path from "path";
+
+interface Coordinate {
+    x: number,
+    y: number
+}
+
+interface Line {
+    start: Coordinate,
+    end: Coordinate
+}
+
+export function drawLines() {
+    const lines = readFileSync(path.resolve(__dirname, './input.txt'), 'utf-8').split('\r\n');
+
+    const parsedLines = parseLines(lines)
+
+    const gridSize = 1000;
+    const grid: Array<Array<number>> = Array(gridSize).fill(0).map(_ => Array(gridSize).fill(0));
+    drawLinesOnGrid(grid, parsedLines);
+
+    const count = howManyBiggerThanOne(grid);
+    console.log('bigger than one', count);
+
+}
+
+function parseLines(lines: string[]): Line[] {
+    const parsedLines: Line[] = []
+    const regex = /(\d+),(\d+) -> (\d+),(\d+)/;
+    for (const line of lines) {
+        const match = line.match(regex)!;
+        parsedLines.push({
+            start: {x: Number(match[1]), y: Number(match[2])},
+            end: {x: Number(match[3]), y: Number(match[4])}
+        });
+    }
+    return parsedLines;
+}
+
+function drawLinesOnGrid(grid: Array<Array<number>>, lines: Line[]): void {
+    // Only consider lines where x1 === x2 or y1 === y2
+    for (const line of lines) {
+        if (line.start.x === line.end.x) {
+            const countUp = line.start.y < line.end.y;
+            for (let i = line.start.y; countUp ? i < line.end.y + 1 : i > line.end.y - 1; countUp ? i++ : i--) {
+                grid[line.start.x][i] += 1;
+            }
+        } else if (line.start.y === line.end.y) {
+            const countUp = line.start.x < line.end.x;
+            for (let i = line.start.x; countUp ? i < line.end.x + 1 : i > line.end.x - 1; countUp ? i++ : i--) {
+                grid[i][line.start.y] += 1;
+            }
+        }
+    }
+}
+
+function howManyBiggerThanOne(grid: Array<Array<number>>): number {
+    const gridSize = grid.length;
+    let count = 0;
+    for (let x = 0; x < gridSize; x++) {
+        for (let y = 0; y < gridSize; y++) {
+            if (grid[x][y] > 1) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import {dayOne} from "./day1/day1";
 import {dayTwo} from "./day2/day2";
 import {dayThree} from "./day3/day3";
 import {playBingo} from "./day4/bingo";
+import {drawLines} from "./day5/vents";
 
 dayOne();
 dayTwo();
 dayThree();
 playBingo();
+drawLines();
